0e1a9eb46f942d2171ffa0b328eb84437e56a40f
withugetsu <250898108+withugetsu@users.noreply.github.com>
2025-12-22 16:48:21 +0900

    feat: add simple CLI support

--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,49 @@ package main
 import (
 	"context"
 	"encoding/base64"
+	"flag"
 	"log"
 	"net"
 	"os"
-	
+
 	"github.com/withugetsu/kitsune/ciphers"
 	"github.com/withugetsu/kitsune/shadowsocks/client"
 )
 
 func main() {
-	key, err := base64.StdEncoding.DecodeString(os.Args[1])
+	password := flag.String("password", "", "specify the password for the client")
+	method := flag.String("method", ciphers.Method(0).String(), "specify the method for the client")
+	remoteAddr := flag.String("remoteAddr", "", "specify the Shadowsocks server address")
+	localAddr := flag.String("localAddr", "127.0.0.1:1080", "specify the TCP listening address for the client")
+
+	flag.Parse()
+
+	key, err := base64.StdEncoding.DecodeString(*password)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	c := client.NewClient(context.Background(), key, ciphers.AEAD2022BLAKE3AES256GCM)
-	c.RemoteSSAddr = func(conn net.Conn) string {
-		return "127.0.0.1:9999"
+
+	if *remoteAddr == "" {
+		flag.Usage()
+		os.Exit(1)
 	}
-	
-	if err = c.Serve("127.0.0.1:7777"); err != nil {
+
+	m, err := ciphers.ParseMethod(*method)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := client.NewClient(context.Background(), key, m)
+	c.RemoteSSAddr = func(conn net.Conn) string {
+		return *remoteAddr
+	}
+
+	if *localAddr == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err = c.Serve(*localAddr); err != nil {
 		log.Fatal(err)
 	}
 }
