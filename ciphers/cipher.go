26d025d5f89ff1b5b796efd28c88c4d681310898
withugetsu <250898108+withugetsu@users.noreply.github.com>
2025-12-22 16:53:35 +0900

    feat: add String and ParseMethod for cipher parsing in CLI

--- ciphers/cipher.go
+++ ciphers/cipher.go
@@ -4,7 +4,7 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"errors"
-	
+
 	"github.com/zeebo/blake3"
 	"golang.org/x/crypto/chacha20poly1305"
 )
@@ -33,11 +33,37 @@ func (m Method) KeySize() (int, error) {
 	}
 }
 
+func (m Method) String() string {
+	switch m {
+	case AEAD2022BLAKE3AES128GCM:
+		return "2022-blake3-aes-128-gcm"
+	case AEAD2022BLAKE3AES256GCM:
+		return "2022-blake3-aes-256-gcm"
+	case AEAD2022BLAKE3Chacha20Poly1305:
+		return "2022-blake3-chacha20-poly1305,"
+	default:
+		return "unknown"
+	}
+}
+
+func ParseMethod(s string) (Method, error) {
+	switch s {
+	case "2022-blake3-aes-128-gcm":
+		return AEAD2022BLAKE3AES128GCM, nil
+	case "2022-blake3-aes-256-gcm":
+		return AEAD2022BLAKE3AES256GCM, nil
+	case "2022-blake3-chacha20-poly1305":
+		return AEAD2022BLAKE3Chacha20Poly1305, nil
+	default:
+		return Method(-1), ErrKeySize
+	}
+}
+
 type Cipher struct {
 	cipher.AEAD
 	M    Method
 	Salt []byte
-	
+
 	counter Counter
 }
 
@@ -46,16 +72,16 @@ func NewCipher(m Method, key []byte) (*Cipher, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	if len(key) != keySize {
 		return nil, ErrKeySize
 	}
-	
+
 	salt, err := NewSalt(m)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return newCipherWithSalt(m, key, salt, keySize)
 }
 
@@ -64,21 +90,21 @@ func NewCipherWithSalt(m Method, key []byte, salt []byte) (*Cipher, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	if len(key) != keySize {
 		return nil, ErrKeySize
 	}
-	
+
 	if len(salt) != keySize {
 		return nil, ErrSaltSize
 	}
-	
+
 	return newCipherWithSalt(m, key, salt, keySize)
 }
 
 func newCipherWithSalt(m Method, key []byte, salt []byte, keySize int) (*Cipher, error) {
 	sessionSubKey := deriveKey(key, salt, keySize)
-	
+
 	var aead cipher.AEAD
 	if m == AEAD2022BLAKE3AES128GCM || m == AEAD2022BLAKE3AES256GCM {
 		block, err := aes.NewCipher(sessionSubKey)
@@ -96,13 +122,13 @@ func newCipherWithSalt(m Method, key []byte, salt []byte, keySize int) (*Cipher,
 			return nil, err
 		}
 	}
-	
+
 	c := &Cipher{
 		AEAD: aead,
 		M:    m,
 		Salt: salt,
 	}
-	
+
 	return c, nil
 }
 
